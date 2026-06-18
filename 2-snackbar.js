74f13d3e0fb34eb60ba656b3397b23f1c7a23659
Ivan-pes <109224468+Ivan-pes@users.noreply.github.com>
2026-06-18 22:15:58 +0000

    Deploying to gh-pages from @ Ivan-pes/goit-js-hw-10@f09fcf8945404b76bace6df14c92df2d058cb580 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-CZMY8L9r.js";import{i as o}from"./assets/vendor-BbbuE1sJ.js";const m=document.querySelector(".form");m.addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.elements.delay.value),i=t.target.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{i==="fulfilled"?e(s):r(s)},s)}).then(e=>{o.success({message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{o.error({message:`❌ Rejected promise in ${e}ms`,position:"topRight"})}),t.target.reset()});
+//# sourceMappingURL=2-snackbar.js.map
